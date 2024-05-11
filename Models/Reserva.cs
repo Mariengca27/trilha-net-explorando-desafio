715d039f0b87e82f7cedbc0be5ecce6258145a96
Mariana Machado <marimeltywork@outlook.com>
2024-05-10 23:17:26 -0300

    Implementando a classe Reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,18 +15,19 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+        
+           int capacidade = Suite.Capacidade;  
+
+            if (capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
-        }
+                Console.WriteLine("A capacidade da suite é menor que o número de hóspedes.");
+    }
+
+}
 
         public void CadastrarSuite(Suite suite)
         {
@@ -35,9 +36,9 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+        
+        return Hospedes.Count;
+
         }
 
         public decimal CalcularValorDiaria()
@@ -45,16 +46,16 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
+           decimal ValorDiaria = DiasReservados * Suite.ValorDiaria;
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+
+                ValorDiaria = ValorDiaria - (ValorDiaria * 0.1m);
             }
 
-            return valor;
+            return ValorDiaria;
         }
     }
 }
